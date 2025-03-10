050624d2afd04b2e55d9c3a7da41d8282a87bbd5
Kyberpuck <misterpipka881@gmail.com>
2025-03-10 09:21:40 +0300

    moved smooth scroll into js.script

---
+++ script.js
@@ -0,0 +1,34 @@
+function smooth_scroll(){
+     // Плавная прокрутка
+     document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const sectionId = this.getAttribute('href');
+            document.querySelector(sectionId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Изменение активной ссылки при прокрутке
+    window.addEventListener('scroll', function() {
+        let sections = document.querySelectorAll('section');
+        let navLinks = document.querySelectorAll('nav a');
+        
+        sections.forEach(section => {
+            let top = window.scrollY;
+            let offset = section.offsetTop - 150;
+            let height = section.offsetHeight;
+            let id = section.getAttribute('id');
+
+            if(top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('href') === `#${id}`) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    });
+}
